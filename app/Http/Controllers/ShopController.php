2b6d60f3fd0edc8232044fb100019e0fd5064ce3
yasu1224 <yasu1224x@gmail.com>
2020-08-05 17:56:32 +0900

    カートの合計金額と個数

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -18,8 +18,8 @@ class ShopController extends Controller
 
     public function myCart(Cart $cart)
     {
-        $my_carts = $cart->showCart();
-        return view('mycart',compact('my_carts'));
+        $data = $cart->showCart();
+        return view('mycart',$data);
     }
 
     public function addMycart(Request $request,Cart $cart)
@@ -30,9 +30,9 @@ class ShopController extends Controller
        $message = $cart->addCart($stock_id);
 
        //追加後の情報を取得
-       $my_carts = $cart->showCart();
+       $data = $cart->showCart();
 
-       return view('mycart',compact('my_carts' , 'message'));
+       return view('mycart',$data)->with('message',$message);
 
    }
 
@@ -41,8 +41,8 @@ class ShopController extends Controller
        $stock_id=$request->stock_id;
        $message = $cart->deleteCart($stock_id);
 
-       $my_carts = $cart->showCart();
+       $data = $cart->showCart();
 
-       return view('mycart',compact('my_carts', 'message'));
+       return view('mycart',$data)->with('message', $message);
    }
 }
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -14,7 +14,16 @@ class Cart extends Model
     public function showCart()
     {
         $user_id = Auth::id();
-        return $this->where('user_id',$user_id)->get();
+        $data['my_carts'] = $this->where('user_id', $user_id)->get();
+
+        $data['count']=0;
+        $data['sum']=0;
+
+        foreach($data['my_carts'] as $my_cart){
+            $data['count']++;
+            $data['sum'] += $my_cart->stock->fee;
+        }
+        return $data;
     }
 
     public function stock()
