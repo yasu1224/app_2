ab8a487ffd3f215cc96ad63a4998b45ccb8935d7
yasu1224 <yasu1224x@gmail.com>
2020-08-07 17:44:00 +0900

    フッターテンプレート化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,29 @@
        <main class="py-4">
            @yield('content')
        </main>
+
+       <footer class="footer_design">
+
+       @guest
+           <p class="nav-item" style="display:inline;">
+               <a class="nav-link" href="{{ route('login') }}" style="color:#fefefe; display:inline;">{{ __('ログイン') }}</a>
+
+           @if (Route::has('register'))
+
+                   <a class="nav-link" href="{{ route('register') }}" style="color:#fefefe; display:inline;">{{ __('会員登録') }}</a>
+               </p>
+           @endif
+       
+       @endguest
+       <br>
+       <div style="margin-top:24px;">
+       なんでも売ります<br>
+       <p style="font-size:2.4em">Larashop</p><br>
+       </div>
+
+       <p style="font-size:0.7em;">@copyright</p>
+
+   </footer>
    </div>
 </body>
 </html>
